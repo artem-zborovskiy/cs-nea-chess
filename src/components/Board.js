06f2158e625ff8fdf3af1e84dd260987b7c6072d
artem-zborovskiy <artem.zborovskiy.666@gmail.com>
2024-10-08 01:31:32 +0100

    first commit

---
+++ src/components/Board.js
@@ -0,0 +1,28 @@
+import './Board.css'
+import Ranks from './parts/Ranks'
+import Files from './parts/Files'
+
+const Board = () => {
+    const ranks = Array(8).fill().map((x, i) => 8 - i);
+    const files = Array(8).fill().map((x, i) => String.fromCharCode(i + 97));
+
+    const getClassName = (i, j) => {
+        let c = 'tile ';
+        c += (i + j) % 2 === 0 ? 'tile--light' : 'tile--dark';
+        return c;
+    }
+
+    return <div className='board'>
+        <Ranks ranks={ranks} />
+        <div className="tiles">
+            {ranks.map((rank, i) => 
+                files.map((file, j) => 
+                    <div key={file + '-' + rank} className={getClassName(i, j)}>{rank}{file}</div>
+                )
+            )}
+        </div>
+        <Files files={files} />
+    </div>
+}
+
+export default Board
---
+++ src/components/parts/Ranks.js
@@ -0,0 +1,11 @@
+import './Ranks.css'
+
+const Ranks = ({ ranks }) => {
+    return <div className="ranks">
+        {ranks.map((rank) => {
+            return <span key={rank}>{rank}</span>
+        })}
+    </div>
+}
+
+export default Ranks
